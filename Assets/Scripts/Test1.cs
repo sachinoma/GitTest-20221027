e145abddd6eb352715247188425e2f13bddb0c80
hihilkh <hihilkh@gmail.com>
2022-10-27 14:27:12 +0900

    Hello world を SampleScene で出力する

--- Assets/Scripts/Test1.cs
+++ Assets/Scripts/Test1.cs
@@ -7,7 +7,7 @@ public class Test1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print ("Hello world");
     }
 
     // Update is called once per frame
