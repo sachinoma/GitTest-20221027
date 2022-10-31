ad4c2031ea3dea4f63332603cd2ea670dcd04b65
制作科_0343_林建亨 <hihilkh@gmail.com>
2022-11-01 02:56:05 +0900

    変更する

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -7,7 +7,7 @@ public class Testing : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("test");
+        print("test1");
     }
 
     // Update is called once per frame
